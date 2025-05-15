1179fb7027094eb8524395b9706360481af97575
RicciNicholas <riccinicho22@gmail.com>
2025-05-15 16:13:48 +0200

    implemented ex1 task1

--- src/main/scala/tasks/adts/Ex1ComplexNumbers.scala
+++ src/main/scala/tasks/adts/Ex1ComplexNumbers.scala
@@ -20,11 +20,20 @@ object Ex1ComplexNumbers:
   object BasicComplexADT extends ComplexADT:
 
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
-    extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+    type Complex = (Double, Double)
+    def complex(re: Double, im: Double): Complex = (re, im)
+    extension (c: Complex)
+      def re(): Double = c match
+        case Complex(r, _) => r
+      def im(): Double = c match
+        case Complex(_, i) => i
+      def sum(other: Complex): Complex = complex(c.re() + other.re(), c.im() + other.im())
+      def subtract(other: Complex): Complex = complex(c.re() - other.re(), c.im() - other.im())
+      def asString(): String = c match {
+        case Complex(r, i) if r == 0 && i == 0 => "0.0"
+        case Complex(r, i) if r == 0 && i > 0 => i + "i"
+        case Complex(r, i) if r == 0 && i < 0 => "-" + -i + "i"
+        case Complex(r, i) if i == 0 => "" + r
+        case Complex(r, i) if i > 0 => r + " + " + i + "i"
+        case Complex(r, i) if i < 0 => r + " - " + -i + "i"
+      }
